7109387352df2e75240810eab9ba7515accbc1fe
Oliver-Haworth <oliverphaworth@gmail.com>
2025-09-27 15:32:33 +0100

    functional but basic DisBot start

--- DisBotMain.py
+++ DisBotMain.py
@@ -1,17 +1,31 @@
-# Import the os module.
-import os
-# Import the discord module.
 import discord
-from discord.ext import commands
-# Import load_dotenv function from dotenv module.
+import os
+import asyncio
 from dotenv import load_dotenv
-# Loads the .env file that lies on the same level as the main.py.
+
 load_dotenv()
-# Grab the token as string from the .env file.
 
-intents = discord.Intents.all()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+intents = discord.Intents.default()
+bot = discord.Client(intents=intents)
 
-# It will executes the bot with the specified token.
-bot.run("MTQyMTA2MjM0OTAwMzE2MTY1Mw.GBtiPJ.2L4gfpn99SevLvU3s3yQOrg5psSq9oDzt84ooQ")
+@bot.event
+async def on_ready():
+    print("DisBot is now online!")
+
+    await bot.wait_until_ready()
+    channel = bot.get_channel(CHANNEL_ID)
+
+    if channel is None:
+        print("❌Failed CHANNEL_ID.")
+        return
+
+    print(f"✅Pinging{channel.name}...")
+
+    while True:
+        await channel.send("ping")
+        await asyncio.sleep(30)
+        
+bot.run(DISCORD_TOKEN)
